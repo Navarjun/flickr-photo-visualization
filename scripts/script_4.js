af3dd7ffa432e925de29594c903b8463911e3d55
Navarjun <navarjunsingh@gmail.com>
2016-12-10 13:50:52 -0500

    forcelayout complete

--- scripts/script_4.js
+++ scripts/script_4.js
@@ -1,6 +1,7 @@
 var margin = {l: 50, t: 50, r: 50, b: 50};
 var colors = ["black", "gray", "white", "purple", "cyan", "yellow", "blue", "green", "red"];
 // var colors = ["red", "green", "blue", "yellow", "cyan", "purple", "white", "gray", "black"];
+var months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
 
 var getImageURL = function(photoInfo) {
   var photoId = photoInfo.id;
@@ -26,7 +27,7 @@ var dataLoaded = function(err, photos) {
   tempW = d3.select(".canvas-boston").node().parentNode.offsetWidth;
   var canvasBoston = d3.select(".canvas-boston")
     .attr("width", tempW)
-    .attr("height", tempW)
+    .attr("height", window.innerHeight)
     .style("background", "#eee")
     .append("g")
     .attr("transform", "translate("+(margin.l)+","+(margin.t)+")");
@@ -55,8 +56,6 @@ function drawData(photos, plot) {
   var desiredHeight = parent.attr("height") - margin.t - margin.b,
     desiredWidth = parent.attr("width") - margin.l - margin.r;
 
-
-
   var scaleX = d3.scaleLinear().range([0, desiredWidth]).domain([new Date(2015, 0, 1), new Date(2015, 11, 31, 23, 59, 59)]),
     scaleY = d3.scaleLinear().range([0, desiredHeight]).domain([0, 5625]);
 
@@ -71,6 +70,39 @@ function drawData(photos, plot) {
       .attr("transform", function(d){ return "translate("+d.x+","+d.y+")"; })
   });
 
+  var index = [];
+  var links = [];
+  photos.forEach(function(d, i){
+    var j = index.filter(function(e){ return e.key == d.date.month(); });
+    if (j.length == 0) {
+      j = {key: d.date.month(), index: i};
+
+      d.fx = ((index.length)%4)*(desiredWidth/4) + desiredHeight/7;
+      d.fy = parseInt((index.length)/4)*(desiredHeight/4) + desiredWidth/7;
+      index.push(j);
+    } else {
+      j = j[0];
+      links.push({source: j.index, target: i, value: 10});
+    }
+  });
+  var positionForIndex = function(index) {
+    return {
+      x: ((index)%4)*(desiredWidth/4) + desiredHeight/7,
+      y: parseInt((index)/4)*(desiredHeight/4) + desiredWidth/7
+    };
+  };
+  var labels = plot
+    .append("g")
+    .classed("labelsGroup", true)
+    .selectAll("text")
+    .data(index)
+    .enter()
+    .append("text")
+    .text(function(d){ return months[d.key]; })
+    .attr("fill", "#222")
+    .attr("x", function(_, i){ return positionForIndex(i).x - 20; })
+    .attr("y", function(_, i){ return positionForIndex(i).y - 60; })
+
   var gSelection = plot.append('g')
     .selectAll('g')
     .data(photos)
@@ -81,8 +113,8 @@ function drawData(photos, plot) {
     .append("image")
     .attr("x", 0)
     .attr("y", 0)
-    .attr("width", 20)
-    .attr("height", 20)
+    .attr("width", 10)
+    .attr("height", 10)
     .attr("xlink:href", function(d){ return getImageURL(d); })
     .style("opacity", 1)
     .on("mouseenter", function(d){
@@ -94,25 +126,9 @@ function drawData(photos, plot) {
         .style("opacity", 1);
     });
 
-  var index = [];
-  var links = [];
-  photos.forEach(function(d, i){
-    var j = index.filter(function(e){ return e.key == d.date.month(); });
-    if (j.length == 0) {
-      j = {key: d.date.month(), index: i};
-
-      d.fx = parseInt((index.length)/3)*(desiredWidth/4) + desiredWidth/7;
-      d.fy = ((index.length)%3)*(desiredHeight/4) + desiredHeight/7;
-      index.push(j);
-    } else {
-      j = j[0];
-      links.push({source: j.index, target: i, value: 10});
-    }
-  });
-  console.log("links", links);
   simulation.nodes(photos);
 
   simulation
     .force("link", d3.forceLink(links).strength(0.1))
-    .force("collide", d3.forceCollide(15));
+    .force("collide", d3.forceCollide(5));
 }
